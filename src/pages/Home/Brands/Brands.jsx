2f2e0e8cdcc0f7f23312cec6879ff77e808be5b1
nhmnazmul22 <najmulmonir12345@gmail.com>
2025-12-11 22:43:24 +0600

    create brands section (Not yet completed)

---
+++ src/pages/Home/Brands/Brands.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Brands = () => {
+  return (
+    <section className='pt-20 pb-10'>
+      <div>
+        
+      </div>
+    </section>
+  )
+}
+
+export default Brands
