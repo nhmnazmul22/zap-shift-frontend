1f9b389401f0232c2869bad4866080f0fa1052f4
nhmnazmul22 <najmulmonir12345@gmail.com>
2025-12-12 11:31:07 +0600

    Brands section done with swiper js

--- src/pages/Home/Brands/Brands.jsx
+++ src/pages/Home/Brands/Brands.jsx
@@ -1,13 +1,73 @@
-import React from 'react'
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import amazonLogo from "../../../assets/brands/amazon.png";
+import casioLogo from "../../../assets/brands/casio.png";
+import moonstarLogo from "../../../assets/brands/moonstar.png";
+import randstadLogo from "../../../assets/brands/randstad.png";
+import starLogo from "../../../assets/brands/star.png";
+import startPeopleLogo from "../../../assets/brands/start_people.png";
+import { Autoplay } from "swiper/modules";
+
+const brandsLogo = [
+  amazonLogo,
+  casioLogo,
+  moonstarLogo,
+  randstadLogo,
+  starLogo,
+  startPeopleLogo,
+  amazonLogo,
+  casioLogo,
+  moonstarLogo,
+  randstadLogo,
+  starLogo,
+  startPeopleLogo,
+];
 
 const Brands = () => {
   return (
-    <section className='pt-20 pb-10'>
-      <div>
-        
+    <section className="pt-20 pb-10">
+      <h2 className="text-3xl font-extrabold text-secondary text-center">
+        We've helped thousands of sales teams
+      </h2>
+      <div className="max-w-7xl mx-auto mt-16">
+        <Swiper
+          slidesPerView={2}
+          centeredSlides={true}
+          spaceBetween={10}
+          grabCursor={true}
+          loop={true}
+          autoplay={{
+            delay: 1500,
+            disableOnInteraction: false,
+          }}
+          breakpoints={{
+            640: {
+              slidesPerView: 3,
+              spaceBetween: 30,
+            },
+            768: {
+              slidesPerView: 4,
+              spaceBetween: 40,
+            },
+            1024: {
+              slidesPerView: 6,
+              spaceBetween: 60,
+            },
+          }}
+          modules={[Autoplay]}
+        >
+          {brandsLogo.map((logo, index) => (
+            <SwiperSlide key={index}>
+              <figure className="w-full flex justify-center items-center">
+                <img src={logo} alt="" />
+              </figure>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Brands
+export default Brands;
